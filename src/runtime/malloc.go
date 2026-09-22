8b31cafc6fbab2b3ab437e2a4a26ce538a77d421
anton2920 <anton2920@gmail.com>
2026-09-22 15:38:57 +0300

    runtime: added 'AllocationsAreDisabled' variable

--- src/runtime/malloc.go
+++ src/runtime/malloc.go
@@ -523,10 +523,16 @@ func (c *mcache) nextFree(sizeclass int8) (v gclinkptr, s *mspan, shouldhelpgc b
 	return
 }
 
+var AllocationsAreDisabled bool
+
 // Allocate an object of size bytes.
 // Small objects are allocated from the per-P cache's free lists.
 // Large objects (> 32 kB) are allocated straight from the heap.
 func mallocgc(size uintptr, typ *_type, needzero bool) unsafe.Pointer {
+	if AllocationsAreDisabled {
+		throw("Allocations are disabled!")
+	}
+
 	if gcphase == _GCmarktermination {
 		throw("mallocgc called with gcphase == _GCmarktermination")
 	}
@@ -888,6 +894,7 @@ func persistentalloc(size, align uintptr, sysStat *uint64) unsafe.Pointer {
 
 // Must run on system stack because stack growth can (re)invoke it.
 // See issue 9174.
+//
 //go:systemstack
 func persistentalloc1(size, align uintptr, sysStat *uint64) unsafe.Pointer {
 	const (
