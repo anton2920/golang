fa3f1f57a13f4b3c95a978b14deea82004d88062
anton2920 <anton2920@gmail.com>
2026-09-22 16:17:35 +0300

    cmd/comlile,link: fixed duplicate symbols when importing 'nostd' package

--- src/cmd/compile/internal/gc/reflect.go
+++ src/cmd/compile/internal/gc/reflect.go
@@ -989,7 +989,7 @@ func dtypesym(t *Type) *Sym {
 		dupok = obj.DUPOK
 	}
 
-	if ((myimportpath == "runtime") || (myimportpath == "github.com/anton2920/gofa/nostd")) && (tbase == Types[tbase.Etype] || tbase == bytetype || tbase == runetype || tbase == errortype) { // int, float, etc
+	if ((compiling_runtime != 0) && ((myimportpath == "runtime") || (myimportpath == "github.com/anton2920/gofa/nostd"))) && (tbase == Types[tbase.Etype] || tbase == bytetype || tbase == runetype || tbase == errortype) { // int, float, etc
 		goto ok
 	}
 
@@ -1266,7 +1266,7 @@ func dumptypestructs() {
 	// so this is as good as any.
 	// another possible choice would be package main,
 	// but using runtime means fewer copies in .6 files.
-	if (myimportpath == "runtime") || (myimportpath == "github.com/anton2920/gofa/nostd") {
+	if (compiling_runtime != 0) && ((myimportpath == "runtime") || (myimportpath == "github.com/anton2920/gofa/nostd")) {
 		for i := EType(1); i <= TBOOL; i++ {
 			dtypesym(Ptrto(Types[i]))
 		}
