0db30b68bc534edd1117d58160d71d9297d468d8
anton2920 <anton2920@gmail.com>
2026-09-23 15:45:39 +0300

    cmd/compile,link: fixed build without 'runtime.a'

--- src/cmd/link/internal/loader/loader.go
+++ src/cmd/link/internal/loader/loader.go
@@ -679,8 +679,10 @@ func (l *Loader) resolve(r *oReader, s goobj.SymRef) Sym {
 // symbols.
 func (l *Loader) reportMissingBuiltin(bsym int, reflib string) {
 	bname, _ := goobj.BuiltinName(bsym)
-	log.Fatalf("reference to undefined builtin %q from package %q",
-		bname, reflib)
+	if false {
+		log.Fatalf("reference to undefined builtin %q from package %q",
+			bname, reflib)
+	}
 }
 
 // Look up a symbol by name, return global index, or 0 if not found.
@@ -802,6 +804,9 @@ func (l *Loader) SymAttr(i Sym) uint8 {
 		return 0
 	}
 	r, li := l.toLocal(i)
+	if r == nil {
+		return 0
+	}
 	return r.Sym(li).Flag()
 }
 
@@ -1175,6 +1180,9 @@ func (l *Loader) IsItab(i Sym) bool {
 		return false
 	}
 	r, li := l.toLocal(i)
+	if r == nil {
+		return false
+	}
 	return r.Sym(li).IsItab()
 }
 
@@ -1239,6 +1247,9 @@ func (l *Loader) Data(i Sym) []byte {
 		return nil
 	}
 	r, li := l.toLocal(i)
+	if r == nil {
+		return nil
+	}
 	return r.Data(li)
 }
 
